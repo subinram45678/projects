a9a62226fe01539d8b31a40a4bead53ef900e766
Your Name <your.email@example.com>
2024-11-14 16:50:28 +0530

    first commit

---
+++ aamirtha batch/one.js
@@ -0,0 +1,26 @@
+// let arr=[5,2,-7,3,-1,-5]
+// output=[]
+// for(i=0;i<arr.length;i++){
+//     if(arr[i]<0){
+//         output.push(arr[i])
+
+//     }
+// }
+// console.log(output)
+
+
+
+// 2
+
+
+let a=[5,10,15,20,25,30,35]
+
+for(i=0;i<a.length;i++){
+    if(a[i]%2!=0){
+        a[i]=0
+
+    }
+}
+console.log(a)
+
+
---
+++ advance css/css/boot.js
@@ -0,0 +1,11 @@
+// // console.log("niufhfiqud")
+// let name='subin';
+// let age=25
+
+// console.log("my name is " + name + " and my age is "+ age)
+// age=++age
+// console.log(age++,age)
+
+let name=["one","two","three"]
+name.pop()
+console.log(name)
